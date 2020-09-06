1518861a01d8974a79c4dcf8fdb71138ff976794
medzoarDev <medzoar@gmail.com>
2020-09-06 11:46:22 -0500

    first devBranch commmit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>Welcome to Clark Smith CV</h1>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
